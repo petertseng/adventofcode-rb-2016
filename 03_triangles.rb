6edce5ae991ad9f3c760dd379bf6305f0dac711e
Peter Tseng <pht24@cornell.edu>
2016-12-04 05:28:39 +0000

    Add day 03: Triangles

---
+++ 03_triangles.rb
@@ -0,0 +1,12 @@
+input = ARGF.each_line.map { |l| l.split.map(&method(:Integer)) }
+
+def tri?(a, b, c)
+  a + b > c
+end
+
+puts input.count { |x| tri?(*x.sort) }
+puts input.each_slice(3).sum { |rows|
+  [0, 1, 2].count { |col|
+    tri?(*rows.map { |r| r[col] }.sort)
+  }
+}
