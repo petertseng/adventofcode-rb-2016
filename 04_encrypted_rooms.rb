5649eeaf960cbaa7ec69f6cf32a10e1ccd747975
Peter Tseng <pht24@cornell.edu>
2016-12-05 05:19:59 +0000

    Add day 04: Encrypted Rooms

---
+++ 04_encrypted_rooms.rb
@@ -0,0 +1,20 @@
+ROOM = %r{^(?<name>[-a-z]+)(?<sector>\d+)\[(?<sum>[a-z]{5})\]$}
+
+input = ARGF.each_line.map { |l| ROOM.match(l) }
+
+real = input.select { |i|
+  freq = i[:name].each_char.tally
+  freq.delete(?-)
+  freq.sort_by { |a, b| [-b, a] }.map(&:first).take(5).join == i[:sum]
+}
+
+puts real.sum { |i| Integer(i[:sector]) }
+
+alpha = 'abcdefghijklmnopqrstuvwxyz'.freeze
+
+names = real.map { |r|
+  shift = Integer(r[:sector]) % 26
+  r[:name].tr(alpha, alpha.chars.rotate(shift).join) + r[:sector]
+}
+
+puts names.grep(/north/)
