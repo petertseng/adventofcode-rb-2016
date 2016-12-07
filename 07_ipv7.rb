86fa2615e890c9a013c8e567d5e5bb7cc8e0745f
Peter Tseng <pht24@cornell.edu>
2016-12-07 15:56:36 +0000

    day 07: Use positive lookahead on 0-length matches
    This allows scan to find overlapping matches, interstingly.
    
    From reddit:
    https://www.reddit.com/r/adventofcode/comments/5gy1f2/_/daw0e69/

--- 07_ipv7.rb
+++ 07_ipv7.rb
@@ -1,4 +1,11 @@
 ABBA = /(.)(?!\1)(.)\2\1/
+# positive lookahead lets scan find overlaps.
+# Important note of String#scan:
+# If the pattern contains no groups,
+# each individual result consists of the matched string, $&.
+# That means the entire regex must be a lookahead,
+# rather than, say, /(.)(?=(?!\1).\1)/
+ABA = /(?=((.)(?!\2).\2))/
 HYPERNET = /\[[^\]]+\]/
 
 addrs = ARGF.map { |addr| [
@@ -12,8 +19,6 @@ puts addrs.count { |hypernets, supernets|
 
 puts addrs.count { |hypernets, supernets|
   hypernets.flat_map { |hypernet|
-    hypernet.each_char.each_cons(3).select { |a, b, c|
-      a == c && a != b
-    }
+    hypernet.scan(ABA).map { |m| m.first.chars }
   }.any? { |b, a, _| supernets.include?(a + b + a) }
 }
