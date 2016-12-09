cdc70aa789f424f7e3ff2999011bc39747c3904a
Peter Tseng <pht24@cornell.edu>
2016-12-09 04:06:37 +0000

    Add fetcher
    In light of https://www.reddit.com/r/adventofcode/comments/5has6d being
    posted, I suppose I have to step it up as well...

---
+++ fetch.sh
@@ -0,0 +1,37 @@
+yes=
+
+if [ "$1" = "--yes" ]; then
+  yes=1
+  shift
+fi
+
+day=$1
+
+if [ $# -eq 0 ]; then
+  day=$(date --utc +%-d)
+  echo "OK, we have to guess the day, let's guess $day???"
+else
+  shift
+  if [ "$1" = "--yes" ]; then
+    yes=1
+    shift
+  fi
+fi
+
+daypad=$(seq -f%02g $day $day)
+
+if [ "$yes" = "1" ]; then
+  if [ -f input ]; then
+    echo "THE INPUT ALREADY EXISTS!!!"
+  else
+    curl --cookie session=$(cat secrets/session) -o input http://adventofcode.com/2016/day/$day/input
+  fi
+else
+  curl -o input http://example.com
+fi
+
+if [ -f TEMPLATE.rb ]; then
+  cat TEMPLATE.rb input > $daypad.rb
+elif [ -f t.rb ]; then
+  cat t.rb input > $daypad.rb
+fi
