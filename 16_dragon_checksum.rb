0dbc95b379a2ab5371636924f2bdfeb3248650e1
Peter Tseng <pht24@cornell.edu>
2016-12-16 06:28:58 +0000

    day 16: Use chunking
    Speeds up runtime from about 14.5 seconds to 1.5 seconds

--- 16_dragon_checksum.rb
+++ 16_dragon_checksum.rb
@@ -10,9 +10,37 @@ bit = {?1 => true, ?0 => false}.freeze
 orig_a = input.each_char.map { |c| bit.fetch(c) }.freeze
 
 lengths.each { |disk|
-  a = orig_a.dup
-  a += [false] + a.reverse.map { |x| !x } until a.size >= disk
-  sum = a.take(disk)
-  sum = sum.each_slice(2).map { |x, y| x == y } until sum.size % 2 == 1
-  puts sum.map { |x| x ? ?1 : ?0 }.join
+  # The disk pattern is:
+  # input, dragon, input reversed and negated, dragon, repeat
+  a = orig_a
+  a_rev = a.reverse.map(&:!).freeze
+
+  dragons = []
+  until dragons.size * (a.size + 1) >= disk
+    dragons += [false] + dragons.reverse.map(&:!)
+  end
+
+  # chunk_size: the largest power of 2 that divides disk.
+  # e.g.   272 is 100010000
+  #        271 is 100001111
+  #       ~271 is  11110000
+  # 272 & ~271 is     10000
+  chunk_size = disk & ~(disk - 1)
+  sum_size = disk / chunk_size
+
+  buf = []
+
+  # each character in the final checksum
+  # corresponds to `chunk_size` consecutive characters on disk.
+  puts sum_size.times.map {
+    while buf.size < chunk_size
+      buf.concat(a)
+      buf << dragons.shift
+      buf.concat(a_rev)
+      buf << dragons.shift
+    end
+
+    chunk = buf.shift(chunk_size)
+    1 - chunk.count(true) % 2
+  }.join
 }
