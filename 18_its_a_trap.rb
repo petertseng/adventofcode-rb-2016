e07cbfd0062dfb3bb7dd2ffadb50e78ac3b7017d
Peter Tseng <pht24@cornell.edu>
2016-12-18 07:32:36 +0000

    day 18: calculate blocks of 10 at a time

--- 18_its_a_trap.rb
+++ 18_its_a_trap.rb
@@ -1,3 +1,18 @@
+# Store rows in blocks of this size.
+# 1 = trap, 0 = safe.
+# Within each block, characters on the left are the most significant bits.
+BLOCK = 10
+
+# We'll pre-compute every single block of 12 -> 10,
+# since 4096 entries in a table is easy.
+RULE = (0...(1 << BLOCK + 2)).map { |i|
+  (0...BLOCK).select { |j|
+    (i >> j) & 1 != (i >> j + 2) & 1
+  }.map { |j| 1 << j }.reduce(0, :|)
+}.freeze
+
+SAFE_COUNT = (0...(1 << BLOCK)).map { |i| BLOCK - i.to_s(2).count(?1) }.freeze
+
 rows = begin
   arg = ARGV.find { |x| x.start_with?('-n') }
   arg && Integer(ARGV.delete(arg)[2..-1])
@@ -5,22 +20,22 @@ end
 
 input = (!ARGV.empty? && !File.exist?(ARGV.first) ? ARGV.first : ARGF.read).freeze
 
-# 1 = trap, 0 = safe.
-# Integers rather than booleans will help us later.
 bit = {?^ => 1, ?. => 0}.freeze
-prev_row = input.each_char.map { |c| bit.fetch(c) }
-safe = prev_row.count(0)
+prev_row = input.each_char.each_slice(BLOCK).map { |slice|
+  slice.reduce(0) { |i, c| i << 1 | bit.fetch(c) }
+}
+
+safe = prev_row.sum { |block| SAFE_COUNT[block] }
 
 current_row = Array.new(prev_row.size)
 
 (rows ? [rows - 1] : [39, 399960]).each { |n|
   n.times {
-    window = prev_row[0]
+    window = 0
     current_row.size.times { |i|
-      window = (window << 1 | (prev_row[i + 1] || 0)) & 7
-      is_trap = [1, 4, 3, 6].include?(window)
-      safe += 1 unless is_trap
-      current_row[i] = is_trap ? 1 : 0
+      window = (window << BLOCK | prev_row[i] << 1 | (prev_row[i + 1] || 0) >> BLOCK - 1) & (1 << BLOCK + 2) - 1
+      current_row[i] = RULE[window]
+      safe += SAFE_COUNT[current_row[i]]
     }
     prev_row, current_row = [current_row, prev_row]
   }
