9eee4b44f25c9361df3ad7256e79d30502e9b3f2
Peter Tseng <pht24@cornell.edu>
2016-12-25 03:17:54 +0000

    day 19: Use mathematical solutions
    I wasn't doing these at first since I needed to understand what was
    going on and write the code in a way that's understandable to me.
    
    Since I do understand now, I've put in the code.

--- 19_josephus.rb
+++ 19_josephus.rb
@@ -1,26 +1,40 @@
 N = Integer(!ARGV.empty? && ARGV.first.match?(/^\d+$/) ? ARGV.first : ARGF.read)
 
-# the zero-based index of the survivor
-def survivor_fixed(n, k)
-  (2..n).reduce(0) { |winner, nn| (winner + k) % nn }
-end
+# At n = 1, elf 1 (one-indexed) wins.
+# At n = 2^k, after n/2 turns half the elves are left,
+#   and it's elf 1's turn again.
+# So at all powers of 2, elf 1 wins.
+# Otherwise, write n as 2^k + r.
+# After r turns, the next mover (2r + 1) wins.
+# So, take out the MSB, shift left by 1, add 1.
+puts Integer(N.to_s(2)[1..-1] + ?1, 2)
 
-# the zero-based index of the survivor
+# At n = 1, elf 1 (one-indexed) wins.
+# Eliminated elves start at one-indexed m = (n / 2) + 1
+# and alternate increasing by 1 and 2.
+# Start with 1 if n even, 2 if n odd.
+# That means first survivor is m + 2 if n even, or m + 1 if n odd.
+# At n = 3^k, after 2n/3 turns a third of the elves are left,
+# corresponding to every third elf of the n.
+# This means the elf that just acted will go last and win.
+# (elf 1 is not special here, since elf 1 might die).
+# So at all powers of 3, elf n wins.
+# Otherwise, write n as 3^k + r.
+# After r turns, the previous mover wins.
+# As long as that's less than m, that's r.
+# Otherwise, that's first_survivor + 3 * (r - m)
+#
+# Alternative formulation:
+# if MST is 1, remove the 1.
+# if MST is 2, change it to 1, double the value of all other trits.
 def survivor_variable(n)
-  (2..n).reduce(0) { |winner, nn|
-    # Insert a new elf before 0@n-1 in turn order.
-    # If this new elf becomes 0@n,
-    # the victim's @n-1 number will be (nn - 1 + (nn / 2)) % nn = nn / 2 - 1
-    n_minus_1_would_kill = nn / 2 - 1
-    # We need to maintain the same number of turns between 0@n and the winner.
-    # This is so that the winner W@n-1 will still move at position W
-    # after the first elimination brings n elves to n-1.
-    # So, if the new elf would kill an elf between itself and the winner,
-    # we have to let n-2@n-1 be 0@n instead. 0@n-1 would be 2@n, etc.
-    # Otherwise, the new elf can be 0@n, and 0@n-1 can be 1@n, etc.
-    (winner + (n_minus_1_would_kill > winner ? 1 : 2)) % nn
-  }
+  trits = n.to_s(3)
+  power_of_three = 3 ** (trits.size - 1)
+  return n if n == power_of_three
+  r = n - power_of_three
+  first_kill = n / 2 + 1
+  first_survivor = first_kill + 2 - n % 2
+  r < first_kill ? r : first_survivor + 3 * (r - first_kill)
 end
 
-puts survivor_fixed(N, 2) + 1
-puts survivor_variable(N) + 1
+puts survivor_variable(N)
