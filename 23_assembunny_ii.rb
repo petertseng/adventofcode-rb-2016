1a8bb12abff376cd623dfc8d6d76c7e8ed134b76
Peter Tseng <pht24@cornell.edu>
2016-12-25 05:33:11 +0000

    Assembunny: Support `out` instruction
    Function needs to now return the number of good outs received.

--- 23_assembunny_ii.rb
+++ 23_assembunny_ii.rb
@@ -2,5 +2,5 @@ require_relative 'lib/assembunny'
 assembler = Assembunny::Interpreter.new(ARGF.readlines)
 
 [7, 12].each { |a|
-  puts assembler.run({a: a, b: 0, c: 0, d: 0})[:a]
+  puts assembler.run({a: a, b: 0, c: 0, d: 0})[0][:a]
 }
