c33860996c620eda02aebb9507fadcc604d66d98
Peter Tseng <pht24@cornell.edu>
2016-12-27 01:10:03 +0000

    day 05, day 14: Add Crystal implementations
    These are the most compute-heavy so far, so it's useful to have the
    Crystal versions ready to go.

--- 14_one_time_pad.cr
+++ 14_one_time_pad.cr
@@ -1,29 +1,23 @@
-require 'digest'
+require "digest/md5"
 
 WINDOW = 1000
 NUM_KEYS = 64
 
-# One-pass run through all indices.
-#
-# Unfortunately, I tried to code this while going for the leaderboard,
-# and ended up with a buggy implementation
-# (with quintuplets qualifying themselves as keys)
-#
-# Premature optimisation did not serve me well.
-# Next time, I'll probably try the naive caching approach first.
-
-def pads(inputs)
-  pads = []
+def pads(input, rounds = 0)
+  pads = [] of Int32
   stop = 1.0 / 0.0
 
-  triplets = Hash.new { |h, k| h[k] = [] }
+  triplets = Hash(Char, Array(Int32)).new { |h, k| h[k] = [] of Int32 }
 
   0.step { |i|
-    md5 = inputs[i]
+    md5 = Digest::MD5.hexdigest(input + i.to_s)
+    rounds.times {
+      md5 = Digest::MD5.hexdigest(md5)
+    }
 
     md5.scan(/(.)\1\1\1\1/).each { |(char)|
-      candidates = triplets.delete(char)
-      candidates.select { |n| n + WINDOW >= i }.each { |n|
+      candidates = triplets.delete(char[0])
+      (candidates || [] of Int32).select { |n| n + WINDOW >= i }.each { |n|
         pads << n
         # Still need to check for any numbers undercutting n.
         stop = n + WINDOW if pads.size == NUM_KEYS
@@ -31,26 +25,14 @@ def pads(inputs)
     }
 
     m = (/(.)\1\1/.match(md5))
-    triplets[m[1]] << i if m
+    triplets[m[1][0]] << i if m
 
     return pads.sort if i >= stop
   }
+
+  raise "Unreachable."
 end
 
-real = ARGV.delete('-r')
-input = !ARGV.empty? && !File.exist?(ARGV.first) ? ARGV.first : ARGF.read
+input = ARGV.first
 
-f2017 = ->i {
-  s = input + i.to_s
-  2017.times { s = Digest::MD5.hexdigest(s) }
-  s
-}
-if !real
-  precomputed_file = "#{__dir__}/hashes/md5-2017-#{Digest::SHA256.hexdigest(input)}"
-  f2017 = File.readlines(precomputed_file) if File.exist?(precomputed_file)
-end
-
-[
-  ->i { Digest::MD5.hexdigest(input + i.to_s) },
-  f2017,
-].each { |f| puts pads(f)[NUM_KEYS - 1] }
+{0, 2016}.each { |r| puts pads(input, rounds = r)[NUM_KEYS - 1] }
