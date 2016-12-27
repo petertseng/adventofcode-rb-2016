f07e9d332aeaa8dde2aad6b8b7a966f9b8a4c5e3
Peter Tseng <pht24@cornell.edu>
2016-12-27 01:10:03 +0000

    day 05, day 14: Add Crystal implementations
    These are the most compute-heavy so far, so it's useful to have the
    Crystal versions ready to go.

--- 05_md5_door.cr
+++ 05_md5_door.cr
@@ -1,31 +1,22 @@
-require 'digest'
+require "digest/md5"
 
-real = ARGV.delete('-r')
-input = !ARGV.empty? && !File.exist?(ARGV.first) ? ARGV.first : ARGF.read
-zeroes = '00000'.freeze
+input = ARGV.first
 
-pass = ''
-pass2 = [nil] * 8
+pass = ""
+pass2 = Array(Char?).new(8, nil)
 
 is = 0.step
-if !real
-  # So as not to make Travis run forever:
-  # Indices producing 5 zeroes are precomputed.
-  precomputed_file = "#{__dir__}/hashes/zeroes-#{Digest::SHA256.hexdigest(input)}"
-  is = File.readlines(precomputed_file).map(&:chomp) if File.exist?(precomputed_file)
-end
 
 is.each { |i|
   md5 = Digest::MD5.hexdigest(input + i.to_s)
-  next unless md5.start_with?(zeroes)
-  puts i if real
-  pass << md5[5]
+  next unless md5[0...5] == "00000"
+  pass += md5[5]
   puts pass if pass.size == 8
 
-  pos = Integer(md5[5], 16)
+  pos = md5[5].to_i(base: 16)
   if 0 <= pos && pos < 8 && pass2[pos].nil?
     pass2[pos] = md5[6]
-    break unless pass2.any?(&:nil?)
+    break unless pass2.any?(&.nil?)
   end
 }
 puts pass2.join
